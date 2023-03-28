386d73d6f367f4fcf8349d8161ae290da0ce15d5
sahan026 <129143976+sahan026@users.noreply.github.com>
2023-03-28 12:57:52 +0530

    Add files via upload

---
+++ v1.php
@@ -0,0 +1,14 @@
+<html>
+<style>
+body {
+  background-image: url('sad.JPG');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+<body >
+<font color="white"size="75px">
+<center><h1>success</h1><br>
+</body>
+</html>
